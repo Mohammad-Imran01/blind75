4f3ff7d1f2a104b915ee6a70c7f7522b812aecb0
Mohammad Imran <imranmbhd2412@gmail.com>
2025-03-21 16:50:20 +0530

    Implement DecodeWays class to count the number of ways to decode a string of digits

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <unordered_set>
 #include <cinttypes>
+#include <string.h>
 
 #define STARTS_WITH 786
 
@@ -508,6 +509,64 @@ namespace dp {
 
         return max(res1, res2);
     }
+
+
+//     You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+
+// "1" -> 'A'
+
+// "2" -> 'B'
+
+// ...
+
+// "25" -> 'Y'
+
+// "26" -> 'Z'
+
+// However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
+
+// For example, "11106" can be decoded into:
+
+// "AAJF" with the grouping (1, 1, 10, 6)
+// "KJF" with the grouping (11, 10, 6)
+// The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+// Note: there may be strings that are impossible to decode.
+
+// Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+
+// The test cases are generated so that the answer fits in a 32-bit integer.
+class DecodeWays {
+public:
+    int dp[101];
+    int numDecodings(string s) {
+        memset(dp, -1, sizeof(dp));
+        return help(s, 0);
+    }
+    bool vald(const string& s) {
+        if(s.empty() || s.front() == '0')
+            return false;
+        const int val = stoi(s);
+
+        return val >= 1 && val <= 26;
+    }
+    int help(const string& s, int ind) {
+        if(ind == s.size()) 
+            return 1;
+        if(s[ind] == '0') 
+            return 0;
+
+        if(dp[ind] != -1) 
+            return dp[ind];
+
+        int one = help(s, ind+1);
+        int two = 0;
+        if(ind < s.size()-1 && vald(s.substr(ind, 2))) {
+            two = help(s, ind+2);
+        }
+
+        return dp[ind] = one+two;
+    }
+};
     //--------------------------------------------===============================
 
 
