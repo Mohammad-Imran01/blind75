c7954ac4b62156f8e918d427b12b031ed09c76f3
Mohammad Imran <imranmbhd2412@gmail.com>
2025-04-06 12:12:09 +0530

    Added Unique Paths solutiion

--- main.cpp
+++ main.cpp
@@ -582,30 +582,11 @@ namespace dp
         return max(res1, res2);
     }
 
-    //     You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
-
-    // "1" -> 'A'
-
-    // "2" -> 'B'
-
-    // ...
-
-    // "25" -> 'Y'
-
-    // "26" -> 'Z'
-
-    // However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
-
-    // For example, "11106" can be decoded into:
-
+    // ------------------------------============================================================--------
+    // You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
     // "AAJF" with the grouping (1, 1, 10, 6)
     // "KJF" with the grouping (11, 10, 6)
     // The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
-    // Note: there may be strings that are impossible to decode.
-
-    // Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
-
-    // The test cases are generated so that the answer fits in a 32-bit integer.
     class DecodeWays
     {
     public:
@@ -643,7 +624,36 @@ namespace dp
             return dp[ind] = one + two;
         }
     };
-    //--------------------------------------------===============================
+
+    //--------------------------------------------===============================------------
+    class UniquePaths
+    {
+    private:
+        int dp[101][101];
+        UniquePaths()
+        {
+            memset(dp, -1, sizeof(dp));
+        }
+
+    private:
+        int Solve(int m, int n, int i, int j)
+        {
+            if (i < 0 || j < 0)
+                return 0;
+            if (i == 0 && j == 0)
+                return 1;
+            if (dp[i][j] != -1)
+                return dp[i][j];
+
+            return Solve(m, n, i - 1, j) + Solve(m, n, i, j - 1);
+        }
+
+    public:
+        int Solve(int m, int n)
+        {
+            return Solve(m, n, m - 1, n - 1);
+        }
+    };
 
 } // namespace dp
 
