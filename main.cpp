6485ff041aac174bb4976d6c2e416233b2ac602c
Mohammad Imran <imranmbhd2412@gmail.com>
2025-05-03 19:23:12 +0530

    Add TreeNode structure and MaxDepth class to Tree namespace for binary tree depth calculation

--- main.cpp
+++ main.cpp
@@ -1790,6 +1790,31 @@ namespace String
     };
 } // string
 
+namespace Tree
+{
+    // Definition for a binary tree node.
+    struct TreeNode
+    {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    class MaxDepth
+    {
+    public:
+        int solve(TreeNode *root)
+        {
+            if (!root)
+                return 0;
+            return 1 + max(solve(root->left), solve(root->right));
+        }
+    };
+} // tree
+
 int main()
 {
     LinkedList::Node *head = nullptr;
